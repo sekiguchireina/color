91eb2235a71db4afeec5b348b9c596a832c49d7e
sekiguchireina <98928862+sekiguchireina@users.noreply.github.com>
2022-07-21 01:11:09 +0900

    Update main.py

--- sikikaku/main.py
+++ sikikaku/main.py
@@ -11,24 +11,31 @@ while(True):
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     
+    s_magnification = 0  # 彩度(Saturation)の倍率
+    v_magnification = 1  # 明度(Value)の倍率
+
+    #hsv[:,:,(1)] = hsv[:,:,(1)]*s_magnification  # 彩度の計算
+    #hsv[:,:,(2)] = hsv[:,:,(2)]*v_magnification  # 明度の計算
+    
     hsv_2 = np.copy(hsv)
     hsv_3 = np.copy(hsv)
+    hsv_4 = np.copy(hsv)
 
-    # h>173の画素(赤いとこ)のhを-150
-    hsv_2[:,:, 0] = np.where(hsv[:, :, 0]>173,hsv[:, :, 0]-150,hsv[:, :, 0])
+
+    # h>173の画素(赤いとこ)のhを-155 それ以外は色相そのまま
+    hsv_2[:,:, 0] = np.where(hsv[:, :, 0]>173,hsv[:, :, 0]-155,hsv[:, :, 0])
     # h<5の画素(赤いとこ)のhを+18
     hsv_3[:, :, 0] = np.where(hsv_2[:, :, 0]<5,hsv_2[:, :, 0]+18,hsv_2[:, :, 0])
 
-    bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR) # HSV->BGR変換
+    #赤以外白黒にする
+    hsv_3[:,:,1] = np.where((hsv_3[:, :, 0]>18) & (hsv_3[:, :, 0]<26),hsv[:,:,(1)],hsv_3[:,:,(1)]*s_magnification)
 
-    
-    #グレスケ変換　彩度０にする手もある
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    
+    # HSV->BGR変換
+    bgr = cv2.cvtColor(hsv_3, cv2.COLOR_HSV2BGR)
 
     #フレーム表示
-    cv2.imshow("Frame",gray)
     cv2.imshow("Frame",bgr)
+    
     #Q押されたら終わり
     if cv2.waitKey(1)&0xFF == ord('q'):
         break
